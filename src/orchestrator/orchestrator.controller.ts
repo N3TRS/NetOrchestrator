34314c708cf95725ce3b02751e11020b6e43cb42
Tulio <tuliojr3101@gmail.com>
2026-03-30 21:29:01 -0500

    Deleting the methods to create project from scratch, cause we will use the spring boot api

--- src/orchestrator/orchestrator.controller.ts
+++ src/orchestrator/orchestrator.controller.ts
@@ -1,14 +1,5 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from "@nestjs/common";
+import { Controller, Post, Body } from "@nestjs/common";
 import { OrchestratorService } from "./orchestrator.service";
-import { CreateOrchestratorDto } from "./dto/create-orchestrator.dto";
 import { CreateRunOrchestratorDto } from "./dto/create-run-orchestrator.dto";
 
 @Controller("orchestrator")
@@ -18,13 +9,8 @@ export class OrchestratorController {
     private readonly runProjectDto: CreateRunOrchestratorDto,
   ) {}
 
-  @Post("create")
-  create(@Body() createOrchestratorDto: CreateOrchestratorDto) {
-    return this.orchestratorService.create(createOrchestratorDto);
-  }
-
   @Post("run")
-  run(@Body() runProjectDto: CreateRunOrchestratorDto) {
-    return "Hey";
+  runProject(@Body() runProjectDto: CreateRunOrchestratorDto) {
+    return this.orchestratorService.runningProject(runProjectDto);
   }
 }
