d6948a4e45d15ece10d93b81f47ed29f693171a8
Julian Lopez <julian.lopez-b@mail.escuelaing.edu.co>
2026-04-13 21:46:00 -0500

    feat: adding metrics implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { OrchestratorModule } from './orchestrator/orchestrator.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { MetricsService } from './metrics/metrics.service';
+import { MetricsController } from './metrics/metrics.controller';
+import { MetricsInterceptor } from './metrics/metrics.interceptor';
 
 @Module({
-  imports: [OrchestratorModule]
+  imports: [OrchestratorModule],
+  controllers: [MetricsController],
+  providers: [
+    MetricsService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: MetricsInterceptor,
+    },
+  ],
 })
 export class AppModule { }
