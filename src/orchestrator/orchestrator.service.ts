f836a969fc6353fdaf20ae69003b914eeb48ecc2
Tulio <tuliojr3101@gmail.com>
2026-04-15 09:19:25 -0500

    feat: adding parameter JAVA_VERSION, to search in dockerhub depending on (17,21,25)

--- src/orchestrator/orchestrator.service.ts
+++ src/orchestrator/orchestrator.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import * as k8s from "@kubernetes/client-node";
 import { CreateRunOrchestratorDto } from "./dto/create-run-orchestrator.dto";
-import { Observable } from 'rxjs';
 import { Socket } from 'socket.io';
 
 @Injectable()
@@ -28,7 +27,7 @@ export class OrchestratorService {
       },
       spec: {
         backoffLimit: 0,
-        ttlSecondsAfterFinished: 1800,
+        ttlSecondsAfterFinished: 600,
         template: {
           metadata: {
             labels: {
@@ -40,7 +39,7 @@ export class OrchestratorService {
             containers: [
               {
                 name: containerName,
-                image: 'tulio3101/omni-maven:v3',
+                image: `tulio3101/omni-maven-${runProjectDto.JAVA_VERSION}:v1`,
                 imagePullPolicy: 'Always',
                 args: [
                   runProjectDto.REPO_URL
